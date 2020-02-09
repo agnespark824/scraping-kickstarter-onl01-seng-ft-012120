d29a1851c88ac2d5d0476e5cc398d7b5f5c32271
Agnes Park <agnespark824@gmail.com>
2020-02-09 21:46:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,24 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  
+#scrape 20 previews of projects in the NYC area
+
+#collect title, image, short description, location, and funding details for each project
+
+#build hash for each project with above details
+
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  kickstarter.css(".project").each do |project|
+    project = Hash.new
+    project.title = 
+    project.image =
+    project.image =
+    project.short_description =
+    project.location =
+    project.funding_details = 
+  end
 end
